a95c40ef9321f348b50a28c6f6fa80fd0351cab6
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-06-07 19:45:42 +0530

    Feat: seraialization-deserialization :tada: :zap:

---
+++ Serialization-and-Seserialization/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+
+
+namespace SerializationDemo
+{
+    public class Purchase
+    {
+        public string? ProductName { get; set; }
+        public DateTime DateTime { get; set; }
+        public decimal PurchasePrice { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Purchase purchase = new Purchase
+            {
+                ProductName = "Orange Juice",
+                DateTime = DateTime.Now,
+                PurchasePrice = 2.49m
+            };
+
+            // Serialization
+            // dotnet object into JSON format
+
+            string jsonString = JsonSerializer.Serialize(purchase);
+
+            // Deserialization
+            // JSON to C# object
+            Purchase purchase1 = JsonSerializer.Deserialize<Purchase>(jsonString);
+        }
+    }
+}
